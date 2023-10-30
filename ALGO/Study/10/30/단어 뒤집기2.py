8f90c908e87b53ef6c91dda5bc4e854146eedad4
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-10-30 19:11:02 +0900

    "스터디 단어 뒤집기2 풀이 "

--- ALGO/Study/10/30/단어 뒤집기2.py
+++ ALGO/Study/10/30/단어 뒤집기2.py
@@ -6,6 +6,7 @@
 1. 알파벳 소문자('a'-'z'), 숫자('0'-'9'), 공백(' '), 특수 문자('<', '>')로만 이루어져 있다.
 2. 문자열의 시작과 끝은 공백이 아니다.
 3. '<'와 '>'가 문자열에 있는 경우 번갈아가면서 등장하며, '<'이 먼저 등장한다. 또, 두 문자의 개수는 같다.
+
 태그는 '<'로 시작해서 '>'로 끝나는 길이가 3 이상인 부분 문자열이고, 
 '<'와 '>' 사이에는 알파벳 소문자와 공백만 있다. 
 단어는 알파벳 소문자와 숫자로 이루어진 부분 문자열이고, 연속하는 두 단어는 공백 하나로 구분한다. 
@@ -27,4 +28,39 @@ baekjoon online judge
 --------
 noojkeab enilno egduj
 
+예제 입력2
+--------
+
 '''
+import sys
+input = sys.stdin.readline
+S = input().rstrip()
+word = ''
+result = ''
+flag = False
+
+for i in S:
+    if i == '<':
+        result += word[::-1] +'<'
+        word = ''
+        flag = True
+    elif i == '>':
+        flag = False
+        result += word[::-1] + '>'
+        word = ''
+    elif flag:
+        result += i
+    else:
+        if i == ' ':
+            result += word[::-1] + ' '
+            word = ''
+        else:
+            word += i
+
+result += word[::-1]
+print(result)
+
+    
+
+
+
