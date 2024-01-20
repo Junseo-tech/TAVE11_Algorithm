1420e667bddbb6bbd174a25b13c7d9fa86ea52a0
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-01-20 23:45:15 +0900

    "BOJ 14584 암호 해독"

--- ALGO/Study/24/01/11/이중우선순위큐.py
+++ ALGO/Study/24/01/11/이중우선순위큐.py
@@ -12,7 +12,11 @@ def solution(operations):
                 hq.heappop(answer)
         if oper[0] == "D" and int(oper[1]) == 1:
             if answer:
-                answer.remove(max(answer))
+                #answer.remove(max(answer))
+                answer = [-x for x in answer]
+                hq.heapify(answer)
+                hq.heappop(answer)
+                answer = [-x for x in answer]
                 hq.heapify(answer)
                             
     return [max(answer), min(answer)] if answer else [0,0]
