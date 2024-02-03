a4e8577416df720594dfc510a5e13083248606e6
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-02-03 23:07:02 +0900

    "BOJ 2164 카드 2 풀이"

---
+++ ALGO/Study/24/02/03/카드2.py
@@ -0,0 +1,15 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+N = int(input())
+card = deque()
+
+for i in range(1,N+1):
+    card.append(i)
+
+while len(card) != 1:
+    card.popleft()
+    card.append(card.popleft())
+
+print(card.pop())
+
