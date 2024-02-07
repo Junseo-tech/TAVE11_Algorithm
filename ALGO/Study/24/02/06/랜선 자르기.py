1e96d91dfe95ffa8d0cb7abfc5694967ad808de7
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-02-08 00:47:17 +0900

    "BOJ 1654 랜선자르기"

--- ALGO/Study/24/02/06/랜선 자르기.py
+++ ALGO/Study/24/02/06/랜선 자르기.py
@@ -5,20 +5,19 @@ K, N = map(int, input().split())
 lines = []
 for _ in range(K):
     lines.append(int(input()))
-lines.sort()
 
-min = 0
-max = len(lines) - 1
-mid = (min + max) // 2
-count = 0
+min_value = 1
+max_value = max(lines) #802
 
-while min < max :
+
+while min_value <= max_value : # 1 6 6 같은 인풋 처럼, max랑 min이 같아야 하는 순간이 있다. 
+    count = 0
+    mid = (min_value + max_value) // 2
     for line in lines:
         count += line // mid
     if count >= N :
-        max -= 1
-        mid = (min + max) // 2
-    else:
-        min += 1
-        mid = (min + max) // 2
-print(count)
+        min_value = mid + 1
+    if count < N:
+        max_value = mid - 1
+
+print(max_value)
