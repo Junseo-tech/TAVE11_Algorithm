bbbf11166b176a299d899812c0e3b26de2b1568c
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-02-19 23:20:35 +0900

    "내가 곧 수정해줄게 미안하다"

--- ALGO/Study/24/02/18/트리의 지름.py
+++ ALGO/Study/24/02/18/트리의 지름.py
@@ -1,4 +1,23 @@
 import sys
 input = sys.stdin.readline
-
 n = int(input())
+graph = [[] for _ in range(n + 1)]
+for _ in range(n-1):
+    parent,child,weight = map(int, input().split())
+    graph[parent].append((child, weight))
+    graph[child].append((parent, weight))
+
+visited = [False] * (n+1)
+
+global count
+def dfs(v):
+    visited[v] = True
+    count = 0
+    for i in graph[v]:
+        child, weight = i
+        if not visited[child]:
+            dfs(child)
+            count += weight
+    return count
+            
+print(dfs(1))
