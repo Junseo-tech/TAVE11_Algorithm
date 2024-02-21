826b9bcf46220695aaf3e25821919127689bf9be
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-02-22 00:34:50 +0900

    "프로그래머스 피로도 풀이"

---
+++ ALGO/Study/24/02/21/피로도.py
@@ -0,0 +1,20 @@
+from itertools import permutations
+def solution(k, dungeons):
+    ans_list = []
+    store = k
+    for perm in permutations(dungeons, len(dungeons)):
+        count = 0
+        k = store
+        for per in perm:
+            min_cost , pay = per
+            if min_cost > k:
+                continue
+            k = k - pay
+            count += 1
+        ans_list.append(count)
+    answer = max(ans_list)  
+    return answer
+k = 80
+dungeons = [[80,20],[50,40],[30,10]]
+#solution(k,dungeons)
+print(solution(k,dungeons))
