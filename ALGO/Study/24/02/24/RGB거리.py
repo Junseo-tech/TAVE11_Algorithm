402cdea313b595071ba330b38294df4b4032725a
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-02-25 23:54:16 +0900

    "RGB 거리 문제 풀이 (중) & SW 마에스트로 문제"

---
+++ ALGO/Study/24/02/24/RGB거리.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+dp = [[0] * 3 for _ in range(N)]
+rgb = []
+for _ in range(N):
+    ls = list(map(int, input().split()))
+    rgb.append(ls)
+
+    
+
+
+        
+
