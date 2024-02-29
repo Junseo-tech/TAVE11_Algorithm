2d56ddf2c9f636a862b8722f953890302b836fea
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-02-29 19:02:02 +0900

    "BOJ RGB 거리 풀이"

--- ALGO/Study/24/02/24/RGB거리.py
+++ ALGO/Study/24/02/24/RGB거리.py
@@ -1,7 +1,15 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+graph = []
+for _ in range(N):
+    graph.append(list(map(int,input().split())))
 
+print(graph)
+print(" ")
+for i in range(1,N):
+    graph[i][0] = min(graph[i-1][1], graph[i-1][2]) + graph[i][0]
+    graph[i][1] = min(graph[i-1][0], graph[i-1][2]) + graph[i][1]
+    graph[i][2] = min(graph[i-1][0], graph[i-1][1]) + graph[i][2]
 
-    
-
-
-        
-
+print(graph)
