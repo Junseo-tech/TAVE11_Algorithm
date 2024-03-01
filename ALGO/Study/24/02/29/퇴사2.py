e7dd48995cf7e26a3ca78613a7033c7fb66f18f5
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-03-01 18:10:36 +0900

    "퇴사2 공부완"

--- ALGO/Study/24/02/29/퇴사2.py
+++ ALGO/Study/24/02/29/퇴사2.py
@@ -3,7 +3,7 @@ input = sys.stdin.readline
 N = int(input())
 work = [list(map(int, input().split())) for _ in range(N)] + [[0,0]] + [[0,0]]
 dp = [0] * (N + 2)
-max_num = -1
+max_num = -1 # max_num은 각 dp에 들어갔을 때 dp에 누적 되어 있는 값을 표기 하기 위해 꼭 필요하다 (직접 적어보면 이해 됨)
 for i in range(N+1):
     if max_num < dp[i]:
         max_num = dp[i]
