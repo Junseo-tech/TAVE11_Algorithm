a6bde9a91fc20754e4d2505b391f0de80ec5ed82
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-03-15 21:57:10 +0900

    "아 모르겠다"

--- ALGO/Study/24/03/13/IF문 좀 대신 써줘.py
+++ ALGO/Study/24/03/13/IF문 좀 대신 써줘.py
@@ -2,3 +2,27 @@ import sys
 input = sys.stdin.readline
 
 N,M = map(int, input().split())
+power = {}
+
+for _ in range(N):
+    level, number = map(str, input().split())
+    power[int(number)] = level
+
+players = [int(input()) for _ in range(M)]
+players.sort()
+stat = list(power.keys())   
+
+for player in players:
+    start = 0
+    end = len(stat) - 1
+    result = 0
+    while start <= end:
+        mid = (start + end) // 2 # N이 홀수 (ex.3) 이면 반올림 안된 값으로 (1)
+        if stat[mid] >= player:
+            result = mid
+            end = mid - 1
+        else:
+            start = mid + 1
+    print(power[stat[result]])
+
+    
