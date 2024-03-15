3d1d9d17c5414a1ad7241c18a300377626624c1b
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-03-15 22:43:12 +0900

    "풀이 완료"

--- ALGO/Study/24/03/13/IF문 좀 대신 써줘.py
+++ ALGO/Study/24/03/13/IF문 좀 대신 써줘.py
@@ -1,28 +1,28 @@
 import sys
 input = sys.stdin.readline
-
 N,M = map(int, input().split())
-power = {}
-
+power_list = []
 for _ in range(N):
-    level, number = map(str, input().split())
-    power[int(number)] = level
+    power, level = map(str, input().split())
+    power_list.append((power, level))
 
-players = [int(input()) for _ in range(M)]
-players.sort()
-stat = list(power.keys())   
+power_list.sort(key = lambda x : int(x[1]))
+players = list(int(input()) for _ in range(M))
 
 for player in players:
     start = 0
-    end = len(stat) - 1
+    end = len(power_list) - 1
     result = 0
     while start <= end:
-        mid = (start + end) // 2 # N이 홀수 (ex.3) 이면 반올림 안된 값으로 (1)
-        if stat[mid] >= player:
+        mid = (start+end) // 2
+        if int(power_list[mid][1]) >= player:
             result = mid
             end = mid - 1
         else:
             start = mid + 1
-    print(power[stat[result]])
+    print(power_list[result][0])
+
+
+
 
     
