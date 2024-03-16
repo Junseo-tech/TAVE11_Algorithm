252d1630aad79ed0957ca32992c1db9e4e9f9d5d
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-03-17 01:40:23 +0900

    "주식 풀이"

---
+++ ALGO/Study/24/03/16/주식.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    stock = list(map(int, input().split()))
+    max_price = 0
+    money = 0
+    for i in range(N-1,-1,-1):
+        if stock[i] > max_price:
+            max_price = stock[i]
+        money += max_price - stock[i]
+    print(money)
