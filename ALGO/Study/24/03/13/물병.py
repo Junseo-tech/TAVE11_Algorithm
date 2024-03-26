a005e02d35ea358097e47fb8e930f3834a6d1522
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-03-26 23:59:08 +0900

    "비트 마스킹 풀이 버전"

--- ALGO/Study/24/03/13/물병.py
+++ ALGO/Study/24/03/13/물병.py
@@ -3,9 +3,9 @@ input = sys.stdin.readline
 N,K = map(int,input().split())
 
 answer = 0
-print(bin(N))
+
 while bin(N).count('1') > K:
-    N += 1
-    answer += 1
+    N += (N & -N)
+    answer += (N & -N)
 
 print(answer)
