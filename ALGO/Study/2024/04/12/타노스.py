636c3d70275289680102d537408af84b097c25c2
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-04-13 00:30:57 +0900

    "타노스 풀이"

--- ALGO/Study/2024/04/12/타노스.py
+++ ALGO/Study/2024/04/12/타노스.py
@@ -1,27 +1,14 @@
 import sys
 input = sys.stdin.readline
-from itertools import permutations
-S = input().strip()
-zero = 0
-one = 0
-answer = []
-for i in S:
-    if i == "0":
-        zero += 1
-    else:
-        one += 1
+S = list(input().strip())
+zero = S.count('0') // 2
+one = S.count('1') // 2
 
-zero_half = zero // 2
-one_half = one // 2
+for _ in range(zero):
+    S.pop(-(S[::-1].index('0'))-1)
 
-for i in S:
-    if i == "0" and zero_half != 0:
-        answer.append(i)
-        zero_half -= 1
-    if i == "1" and one_half != 0:
-        one_half -= 1
-        answer.append(i)
+for _ in range(one):
+    S.pop(S.index('1'))
 
-answer.sort()
-print(''.join(map(str, answer)))
 
+print(''.join(S))
