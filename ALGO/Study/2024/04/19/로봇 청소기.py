d4107997a8413802521b9fc164690815c40244e4
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-04-20 00:20:40 +0900

    "로봇 청소기 풀이"

--- ALGO/Study/2024/04/19/로봇 청소기.py
+++ ALGO/Study/2024/04/19/로봇 청소기.py
@@ -6,23 +6,38 @@ N, M = map(int, input().split())
 r,c,d = map(int, input().split())
 
 graph = [list(map(int, input().split())) for _ in range(N)]
-visited = [[False] * (M+1) for _ in range(N)]
 
 # 북 동 남 서
 dx = [-1,0,1,0]
 dy = [0,1,0,-1]
 
 def isExist(x,y):
+    global dx, dy
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-
-count = 0
+        if 0<= nx < N and 0<=ny<M and graph[nx][ny] == 0:
+            return True
+    return False
+ 
 while True:
-    if graph[r][c] == 0:
-        graph[r][c] = 2
-        count += 1
+    graph[r][c] = 2
     if isExist(r,c):
-        pass
+        while True:
+            d = (d+3) % 4
+            if graph[r + dx[d]][c + dy[d]] == 0:
+                r += dx[d]
+                c += dy[d]
+                break
     else:
-        pass
+        if graph[r - dx[d]][c - dy[d]] == 1:
+            break
+        else:
+            r -= dx[d]
+            c -= dy[d]
+
+result = 0
+for i in graph:
+    result += i.count(2)
+
+print(result)
