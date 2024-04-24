3895505db2ac6f5d02fef1f255ad61a30ef37c7a
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-04-25 01:50:24 +0900

    "겹치는 건 싫어 투 포인터 정석 풀이"

---
+++ ALGO/Study/2024/04/24/겹치는 건 싫어_투 포인터 정석 풀이.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+N, K = map(int, input().split())
+sequence = list(map(int, input().split()))
+count = [0] * (max(list) + 1)
+left, right = 0,0
+answer = 0
+
+while right < N:
+    if count[sequence[right]] < K:
+        count[sequence[right]] += 1
+        right += 1
+    else:
+        count[sequence[left]] += 1
+        left += 1
+    answer = max(answer, right-left)
+
+print(answer)
