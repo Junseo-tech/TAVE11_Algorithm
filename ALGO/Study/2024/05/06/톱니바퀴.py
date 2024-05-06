da20f469b2128fe99e4a57b10259f254a9e35bd7
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-05-07 02:47:27 +0900

    "톱니 바퀴 완료"

--- ALGO/Study/2024/05/06/톱니바퀴.py
+++ ALGO/Study/2024/05/06/톱니바퀴.py
@@ -1,16 +1,55 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
-q = deque(list(map(int, input().strip())) for _ in range(4))
+system = []
+for _ in range(4):
+    temp = deque(list(map(int, input().strip())))
+    system.append(temp)
 K = int(input())
 
-def rotation(a):
-    pass
+def is_turn_left(number,l):
+    if number < 0:
+        return False
+    if system[number][2] != l:
+        return True
+
+def is_turn_right(number,r):
+    if number > 3:
+        return False
+    if system[number][6] != r:
+        return True
+
+def turn(direction, number):
+    if direction == -1:
+        system[number].rotate(-1)
+    if direction == 1:
+        system[number].rotate(1)
+
+def move_system(number, direction):
+    if number > 3 and number < 0:
+        return
+    visited[number] = True
+    l , r = system[number][6], system[number][2]
+    turn(direction, number)
+    if is_turn_left(number-1, l) and not visited[number-1]:
+        move_system(number-1, -direction)
+    if is_turn_right(number + 1, r) and not visited[number+1]:
+        move_system(number+1, -direction)
 
 for _ in range(K):
-    a,b = map(int, input().split())
-    if b == 1:
-        q[a].rotate(1)
-    if b == -1:
-        q[a].rotate(-1)
+    a, b = map(int, input().split())
+    visited = [False for _ in range(4)]
+    move_system(a-1, b)
+
+answer = 0
+if system[0][0] == 1:
+    answer += 1
+if system[1][0] == 1:
+    answer += 2
+if system[2][0] == 1:
+    answer += 4
+if system[3][0] == 1:
+    answer += 8
+
+print(answer)
 
