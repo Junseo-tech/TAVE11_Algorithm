275317709d3b94ca90ebecfba314cf90f3d07e24
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-05-09 23:46:25 +0900

    "배열 돌리기 3 풀이"

---
+++ ALGO/Study/2024/05/09/배열 돌리기3.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+def up_down(): # 상하반전
+    pass
+
+def left_right(): # 좌우반전
+    pass
+
+def right(): #오른쪽으로 90
+    pass
+
+def left(): #왼쪽으로 90
+    pass
+
+def part_1(): # 5번
+    pass
+
+def part_2(): # 6번
+    pass
