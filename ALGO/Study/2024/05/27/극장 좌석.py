5d9804f5eedee0c33f4cedb3be371c2a6f67f455
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-05-28 01:33:46 +0900

    "극장 좌석 풀이"

--- ALGO/Study/2024/05/27/극장 좌석.py
+++ ALGO/Study/2024/05/27/극장 좌석.py
@@ -3,23 +3,35 @@ input = sys.stdin.readline
 N = int(input())
 M = int(input()) # 고정석의 갯수
 
-fix = [False for _ in range(N+1)]
+fix = []
 dp = [1 for _ in range(N+1)]
 for _ in range(M):
     num = int(input())
-    fix[num] = True
-# 바로 왼, 오른쪽 자리에 고정석이 있는 것들
-# 앞에가 정해지면 뒤에도 정해진다. 
+    fix.append(num)
 
-dp[0], dp[1] = 1, 1
 answer = 1
 
+
 for i in range(2, N+1):
-        dp[i] = dp[i-1] + dp[i-2]
-                   
+    dp[i] = dp[i-1] + dp[i-2]
+
+
+# for i in range(2, N+1):
+#     if i not in fix:
+#         dp[i] = dp[i-1] + dp[i-2]
+# 아래에서 거르니까 건너 뛰면 안됨. 건너 뛰어서 계산하면 수가 커졌을 때 문제 발생할 수 있다
+
+fixed = 0
+# 4, 7
 if M > 0:
-    pass
+    for seat in fix:
+        if seat - fixed - 1 > 0:
+            answer *= dp[seat-fixed-1]
+        fixed = seat
+    if N - fixed > 0:
+        answer *= dp[N-fixed]
 else:
-    answer = dp[N-1]
-print(dp)
+    answer = dp[N]
+
+#print(dp)
 print(answer)
