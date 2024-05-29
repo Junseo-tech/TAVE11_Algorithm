54c78e0023b4630fc4ebfda3b27aaf134d9c70b1
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-05-29 20:55:02 +0900

    "1,2,3 더하기 3"

---
+++ ALGO/Study/2024/05/29/1,2,3 더하기 3.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+T = int(input())
+dp = [0] * 1000001
+dp[0] = 1
+dp[1] = 1
+dp[2] = 2
+
+for i in range(3, 1000001):
+    dp[i] = (dp[i-3]  + dp[i-2] + dp[i-1]) % 1000000009
+
+for _ in range(T):
+    N = int(input())
+    print(dp[N])
