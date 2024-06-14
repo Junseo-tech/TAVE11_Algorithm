3c5fdcfe0187d356a0321eff08f6cd77b7454e94
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-06-14 23:42:37 +0900

    토마토 풀이

--- ALGO/Study/2024/06/13/오르막 수.py
+++ ALGO/Study/2024/06/13/오르막 수.py
@@ -5,6 +5,6 @@ dp = [1 for _ in range(10)] # 00 01 11 12
 
 for i in range(N-1):
     for j in range(10):
-        dp[j] = sum(dp[j:])
+        dp[j] = sum(dp[j:])  #길이가 2 dp[0] = 10 dp[1] =9 dp[2] = 8
 
 print(sum(dp)%MOD)
